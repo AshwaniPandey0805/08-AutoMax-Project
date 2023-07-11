923002970da33a3516d50281614a2546e6e3f6d3
Ashwani Pandey <ashwanipandey679@gmail.com>
2023-07-11 11:35:33 +0530

    Genrate AutoMax font page

--- src/main/views.py
+++ src/main/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def main_view(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    return render(request, "views/main.html", {'name' : 'AutoMax'})
 
 
