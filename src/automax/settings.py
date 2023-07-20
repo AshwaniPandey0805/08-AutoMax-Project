349c31d0b15d5fb34c9a9e9e96eb7fea10a3184c
Ashwani Pandey <ashwanipandey679@gmail.com>
2023-07-20 16:26:45 +0530

    Working with Django messages.

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/home/'
+LOGIN_URL = 'login/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
     if request.method == 'POST':
@@ -11,9 +12,12 @@ def login_view(request):
             password  = login_form.cleaned_data.get('passsword')
             user = authenticate( username = username, password = password)
             if user is not None:
-                pass
+                login(request, user)
+                messages.success(request, f'Your are logged in as {username}')
+
+                return redirect('home')
             else:
-                pass
+                messages.error(request, f'Unable to login.')
     elif request.method ==  'GET':  
         login_form  = AuthenticationForm()
     return render(request, 'views/login.html', {'login_form' : login_form} )
